a5e2e934fe4c224026483a6aa6ea259c7b5900bc
jayrbarr <jayrbarr@gmail.com>
2020-04-28 21:31:39 -0400

    Installs webpack

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+// app.get('/', (req, res) => res.sendFile('index.html'));
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
