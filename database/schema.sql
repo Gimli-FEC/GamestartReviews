af814c8134e4b28c12a521a86a348332e4b81ced
jayrbarr <jayrbarr@gmail.com>
2020-04-30 15:14:46 -0400

    Completes schema and seeding files ... need to add script yet

---
+++ database/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS gamestart;
+
+CREATE DATABASE gamestart;
+
+USE gamestart;
+
+CREATE TABLE users (
+    id int NOT NULL AUTO_INCREMENT,
+    name TINYTEXT,
+    age TINYINT(4),
+    avatar TINYTEXT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE reviews (
+    id INT NOT NULL AUTO_INCREMENT,
+    product_id INT,
+    user_id INT,
+    date DATE,
+    body TEXT,
+    recommended BOOL,
+    purchase_type BOOL,
+    rating_overall TINYINT(5),
+    rating_graphics TINYINT(5),
+    rating_gameplay TINYINT(5),
+    PRIMARY KEY (id),
+    INDEX u_id (user_id),
+    FOREIGN KEY (user_id)
+        REFERENCES users(id)
+);
+
+/*    (sudo) mysql -u <USER> (-p) < schema.sql  */
