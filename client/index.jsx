b7fe8bde45bc89d4d22d50499bb9cc11d5642e81
jayrbarr <jayrbarr@gmail.com>
2020-04-29 20:59:47 -0400

    Installs babel, react - connects to webpack, express, client

---
+++ client/index.jsx
@@ -0,0 +1,14 @@
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>Works!!!</div>
+        )
+    }
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
