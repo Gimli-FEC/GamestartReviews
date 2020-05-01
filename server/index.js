dbce91b4572ab778400a61c88cccbd787459beb1
jayrbarr <jayrbarr@gmail.com>
2020-05-01 17:08:58 -0400

    Implements style guides and corrects style errors in existing files.

--- server/index.js
+++ server/index.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const path = require('path');
 const db = require('../database');
+
 const app = express();
 const PORT = 3003;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
 
-app.get('/:id', (req, res) => {
-    let id = req.params.id;
-    db.getReviews(id, (err, data) => {
-        if (err) {
-            res.sendStatus(500);
-        } else {
-            res.send(data);
-        }
-    })
-    
-})
+app.get('/:id', ({ id }, res) => {
+  db.getReviews(id, (err, data) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.send(data);
+    }
+  });
+});
