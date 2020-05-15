6648605e1f7ee5a8081f0b86f305d304a2cde751
jayrbarr <jayrbarr@gmail.com>
2020-05-15 17:31:03 -0400

    Changes API_URL for direct link to EC2 instance.

--- client/components/api.js
+++ client/components/api.js
@@ -1,3 +1,3 @@
-const API_URL = ''; // if local service with proxy, http://localhost:3003
+const API_URL = 'http://ec2-3-84-17-164.compute-1.amazonaws.com:3003'; // if local service with proxy, http://localhost:3003
 
 export default API_URL;
