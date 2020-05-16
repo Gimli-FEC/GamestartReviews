1dda7afb3c368543aa848537a89ad13e8636c165
Jay Barr <jayrbarr@gmail.com>
2020-05-16 10:10:39 -0400

    Revert client API path as well

--- client/components/api.js
+++ client/components/api.js
@@ -1,3 +1,3 @@
-const API_URL = ''; // if local service with proxy, http://localhost:3003
+const API_URL = 'http://ec2-52-91-162-130.compute-1.amazonaws.com:3003'; // if local service with proxy, http://localhost:3003
 
 export default API_URL;
