9289d89806919c0111b164cfe3804bf1976af735
Jay Barr <jayrbarr@gmail.com>
2020-05-18 09:38:52 -0400

    Adds static Average Customer Ratings panel

---
+++ client/components/AverageBars.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Row = styled.div`
+  display: flex;
+  margin: 5px 5px 5px 2px;
+  width: 75%;
+  span:first-of-type {
+    width: 15%;
+  }
+  meter {
+    width: 100%;
+    height: 10px;
+    margin: auto;
+  }
+  meter::-webkit-meter-optimum-value {
+    background: #da291c;
+  }
+  span:nth-of-type(2) {
+    width: 100px;
+    text-align: center;
+  }
+`;
+
+const AverageBars = ({name, rating}) => {
+  const width = rating/5 * 100;
+  return (
+    <Row><span>{name}</span><meter value={width}/><span>{rating}</span></Row>
+  );
+}
+
+export default AverageBars;
--- client/components/AverageRatings.jsx
+++ client/components/AverageRatings.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import AverageBars from './AverageBars';
+import AverageStars from './AverageStars';
 
 const Container = styled.div`
   margin: 5px;
@@ -10,10 +12,16 @@ const Title = styled.p`
   font-weight: bold;
 `;
 
-const AverageRatings = () => (
-  <Container>
-    <Title>Average Customer Ratings</Title>
-  </Container>
-)
+const AverageRatings = () => {
+  const bars = [{name: "Graphics", rating: 3.5}, {name: "Gameplay", rating: 4.2}, {name: "Lasting Appeal", rating: 4.7}];
+
+  return (
+    <Container>
+      <Title>Average Customer Ratings</Title>
+      <AverageStars rating="4.8" />
+      {bars.map((bar, index) => <AverageBars key = {index.toString()} name={bar.name} rating={bar.rating} />)}
+    </Container>
+  )
+}
 
 export default AverageRatings;
